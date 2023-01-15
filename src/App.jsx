75b722880442b902a1d93ead36362ac31d680bf0
Lili <liliana.zurita@gmail.com>
2023-01-15 00:30:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Header from './components/Header';
+
+
+const App = ( ) => {
+
+  return(
+    <Header titulo={"Galeria de imagenes"}/>
+  );
+};
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = ({titulo:titulo}) => {
+return(
+    <h2>{titulo}</h2>
+);
+};
+export default Header;
